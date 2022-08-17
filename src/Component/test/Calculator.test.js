319f407b36e6468ee707978752192aa591d7ec1c
aron-helu <aronmilahela@gmail.com>
2022-08-17 18:53:58 +0300

    all the component jest snapshots

---
+++ src/Component/test/Calculator.test.js
@@ -0,0 +1,7 @@
+import renderer from 'react-test-renderer';
+import Calculator from '../Calculator';
+
+it('render correctly', () => {
+    const tree = renderer.create(<Calculator />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
